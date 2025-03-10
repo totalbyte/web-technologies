c79149c0f9eec1c24267adccf936b3ffa833996f
Vlad Antoniuk <vladantonuk2005@gmail.com>
2025-03-10 08:57:18 +0200

    old labs and new one

---
+++ js/1/script.js
@@ -0,0 +1,7 @@
+document.getElementById("message1").textContent = "Hto ya?";
+document.getElementById("myButton").onclick = showName;
+
+function showName() {
+    const studName = "Volodymyr Zelenskyy";
+    console.log(studName);
+}
